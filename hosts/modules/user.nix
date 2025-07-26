4d28d81647941d2607d5d06d6114d93d527eaca7
Ivan Richter <richtivan@gmail.com>
2025-07-26 18:20:11 +0200

    feat(services): enable Syncthing system service and user setup

--- hosts/modules/user.nix
+++ hosts/modules/user.nix
@@ -5,7 +5,17 @@
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = [ "wheel" "networkmanager" "docker" ];
+    packages = with pkgs; [
+      syncthing
+    ];
   };
 
   programs.zsh.enable = true;
+
+  services.syncthing = {
+    enable = true;
+    user = "ivan";
+    dataDir = "/home/ivan";
+    configDir = "/home/ivan/.config/syncthing";
+  };
 }
