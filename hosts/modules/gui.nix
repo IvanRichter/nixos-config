950ee323dbe7bb33fe37066ba27b6a1cae1b9758
Ivan Richter <richtivan@gmail.com>
2025-07-28 07:19:21 +0200

    fix(gui): enforce Chrome Wayland scaling via env vars

--- hosts/modules/gui.nix
+++ hosts/modules/gui.nix
@@ -16,4 +16,20 @@
 
   services.flatpak.enable = true;
   programs.xwayland.enable = true;
+
+  # Fix blurry Chrome on Wayland by disabling scaling
+  environment.sessionVariables = {
+
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+
+
+    CHROME_VERSION_EXTRA_FLAGS = "--force-device-scale-factor=1 --enable-features=WaylandWindowDecorations";
+  };
+
+  environment.variables = {
+    GDK_SCALE = "1";
+    GDK_DPI_SCALE = "1.0";
+    XCURSOR_SIZE = "24";
+  };
 }
