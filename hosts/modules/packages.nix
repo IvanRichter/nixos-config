8304c19b55c79c0efde3986d81f5e541d37c5775
IvanRichter <richtivan@gmail.com>
2025-08-28 16:30:02 +0200

    feat(graphics-apps): add GIMP

--- hosts/modules/packages.nix
+++ hosts/modules/packages.nix
@@ -6,5 +6,6 @@
     (import ./packages/cli.nix { inherit pkgs; }) ++
     (import ./packages/comms.nix { inherit pkgs; }) ++
     (import ./packages/development.nix { inherit pkgs; }) ++
-    (import ./packages/eid.nix { inherit pkgs; });
+    (import ./packages/eid.nix { inherit pkgs; }) ++
+    (import ./packages/graphics-apps.nix { inherit pkgs; });
 }
