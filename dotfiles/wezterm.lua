b6bde34e61c86429d7546d5a9b8e2a5409a69c23
IvanRichter <richtivan@gmail.com>
2025-09-21 11:48:29 +0200

    chore(wezterm): tweak window opacity, cursor style and decorations

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -1,15 +1,18 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
 local config = {}
 
-config.color_scheme = "Tokyo Night"
-config.font = wezterm.font_with_fallback({ "JetBrains Mono", "Noto Sans Mono" })
+config.color_scheme = 'Tokyo Night'
+config.font = wezterm.font_with_fallback({ 'JetBrains Mono', 'Noto Sans Mono' })
 config.font_size = 12.5
+config.default_cursor_style = 'SteadyBar'
 
 config.enable_tab_bar = true
-config.window_decorations = "TITLE|RESIZE"
+config.window_decorations = 'TITLE|RESIZE'
 
-config.window_background_opacity = 0.85
+config.window_background_opacity = 0.8
 config.text_background_opacity = 1.0 
-config.kde_window_background_blur = true
+
+config.initial_cols = 128
+config.initial_rows = 32
 
 return config
