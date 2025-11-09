673925aa61c4301ab3ae006dcf381e82cc88c5b2
Ivan Richter <ivan.richter@anriku.com>
2025-11-09 19:18:37 +0100

    feat(docker): enable BuildKit and binfmt for x86_64 on aarch64

--- hosts/modules/docker.nix
+++ hosts/modules/docker.nix
@@ -1,3 +1,23 @@
-{ config, pkgs, ... }: {
-  virtualisation.docker.enable = true;
+{ config, lib, pkgs, ... }:
+let
+  isAarch64 = pkgs.stdenv.hostPlatform.system == "aarch64-linux";
+in
+{
+  virtualisation.docker = {
+    enable = true;
+    # Make BuildKit the default
+    daemon.settings.features.buildkit = true;
+  };
+
+  # Register QEMU binfmt so amd64 containers/binaries run on ARM
+  boot.binfmt = lib.mkIf isAarch64 {
+    emulatedSystems = [ "x86_64-linux" ];
+  };
+
+  # Tools
+  environment.systemPackages = with pkgs; [
+    docker
+    docker-buildx
+    qemu-user
+  ];
 }
