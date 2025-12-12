e4ad5451f092764cf7ee1598f83c48eb0c146ab3
Ivan Richter <ivan.richter@anriku.com>
2025-12-12 09:59:17 +0100

    perf(razer): avoid loading unused kernel modules

--- hosts/desktop/razer.nix
+++ hosts/desktop/razer.nix
@@ -4,6 +4,18 @@
   # Kernel + udev support for Razer
   hardware.openrazer.enable = true;
 
+  # Avoid loading unused Razer kernel modules
+  boot.blacklistedKernelModules = [
+    "razercore"
+    "razerfirefly"
+    "razermug"
+  ];
+  boot.extraModprobeConfig = ''
+    install razercore /bin/true
+    install razerfirefly /bin/true
+    install razermug /bin/true
+  '';
+
   # GUI
   environment.systemPackages = with pkgs; [
     openrazer-daemon
