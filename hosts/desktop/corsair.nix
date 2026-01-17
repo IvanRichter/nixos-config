c54093a19315869ec1a4e971dd91291455fe95fc
Ivan Richter <ivan.richter@anriku.com>
2026-01-17 05:47:31 +0100

    fix(hosts/desktop/corsair): drop ckb-next override post upstream fix

--- hosts/desktop/corsair.nix
+++ hosts/desktop/corsair.nix
@@ -3,9 +3,5 @@
 {
   hardware.ckb-next = {
     enable = true;
-    # Temporary workaround pending PR #446679
-    package = pkgs.ckb-next.overrideAttrs (old: {
-      cmakeFlags = (old.cmakeFlags or [ ]) ++ [ "-DUSE_DBUS_MENU=0" ];
-    });
   };
 }
