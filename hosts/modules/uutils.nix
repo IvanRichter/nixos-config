9d2faf1b1c602c9876435ca0c9c005a270299d11
Ivan Richter <ivan.richter@anriku.com>
2026-02-03 20:53:16 +0100

    fix(uutils): use mv -f for /bin/sh and /usr/bin/env to suppress activation mv prompts

--- hosts/modules/uutils.nix
+++ hosts/modules/uutils.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 # Replace runtime utils only
 let
   coreutilsFullName =
@@ -26,6 +31,33 @@ let
     );
 in
 {
+  # Avoid interactive mv prompts during activation when replacing /bin/sh and /usr/bin/env
+  system.activationScripts.binsh = lib.mkForce (
+    lib.stringAfter [ "stdio" ] ''
+      # Create the required /bin/sh symlink
+      mkdir -p /bin
+      chmod 0755 /bin
+      ln -sfn "${config.environment.binsh}" /bin/.sh.tmp
+      ${pkgs.uutils-coreutils-noprefix}/bin/mv -f /bin/.sh.tmp /bin/sh
+    ''
+  );
+
+  system.activationScripts.usrbinenv = lib.mkForce (
+    if config.environment.usrbinenv != null then
+      ''
+        mkdir -p /usr/bin
+        chmod 0755 /usr/bin
+        ln -sfn ${config.environment.usrbinenv} /usr/bin/.env.tmp
+        ${pkgs.uutils-coreutils-noprefix}/bin/mv -f /usr/bin/.env.tmp /usr/bin/env
+      ''
+    else
+      ''
+        rm -f /usr/bin/env
+        if test -d /usr/bin; then rmdir --ignore-fail-on-non-empty /usr/bin; fi
+        if test -d /usr; then rmdir --ignore-fail-on-non-empty /usr; fi
+      ''
+  );
+
   system.replaceDependencies.replacements = [
     {
       oldDependency = pkgs.coreutils-full;
