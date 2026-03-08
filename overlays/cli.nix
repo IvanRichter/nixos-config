7873cfe66e8d85307b7af10390e2054acc05a392
Ivan Richter <ivan.richter@anriku.com>
2026-03-08 18:09:06 +0100

    perf(overlays/cli): enable optimized builds for ripgrep, fd, sd, jaq, wl-clipboard-rs, eza, tree, and lsof on desktop

--- overlays/cli.nix
+++ overlays/cli.nix
@@ -6,10 +6,26 @@ let
   isDesktop = prev.stdenv.hostPlatform.system == "x86_64-linux";
 
   rustOpt = import ./helpers/rust-opt.nix { inherit lib pkgs; };
+  cOpt = import ./helpers/c-opt.nix { inherit lib pkgs; };
 
   rustPkgs = [
+    "ripgrep"
+    "fd"
     "zoxide"
     "skim"
+    "sd"
+    "jaq"
+    "wl-clipboard-rs"
+    "eza"
   ];
+
+  cPkgs = [
+    "tree"
+    "lsof"
+  ];
+
 in
-if !isDesktop then { } else lib.genAttrs rustPkgs (n: rustOpt prev.${n})
+if !isDesktop then
+  { }
+else
+  (lib.genAttrs rustPkgs (n: rustOpt prev.${n})) // (lib.genAttrs cPkgs (n: cOpt prev.${n}))
