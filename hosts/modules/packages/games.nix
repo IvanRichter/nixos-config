71a9ec210785bd6689eeaf534f27ede9fb0a5909
Ivan Richter <ivan.richter@anriku.com>
2026-04-12 08:46:50 +0200

    feat(games): add Steam

--- hosts/modules/packages/games.nix
+++ hosts/modules/packages/games.nix
@@ -1,5 +1,25 @@
-{ pkgs }:
-with pkgs;
-[
-  bottles
+{ pkgs, lib, ... }:
+
+let
+  isX86 = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
+in
+lib.mkMerge [
+  {
+    environment.systemPackages = with pkgs; [
+      bottles
+    ];
+  }
+
+  (lib.mkIf isX86 {
+    programs.steam = {
+      enable = true;
+      extest.enable = true;
+      protontricks.enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+    };
+
+    programs.gamemode.enable = true;
+  })
 ]
