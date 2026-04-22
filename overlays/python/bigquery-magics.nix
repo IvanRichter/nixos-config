08adfffd595484435607e553d2e6b1ef5c4dbf5b
IvanRichter <ivan.richter@anriku.com>
2026-04-22 08:56:33 +0200

    chore(overlays/python): update bigquery-magics to 0.14.0 and reflect upstream repo move

--- overlays/python/bigquery-magics.nix
+++ overlays/python/bigquery-magics.nix
@@ -7,15 +7,17 @@ in
     (pythonFinal: pythonPrev: {
       bigquery-magics = pythonFinal.buildPythonPackage rec {
         pname = "bigquery-magics";
-        version = "0.12.1";
+        version = "0.14.0";
         format = "setuptools";
+        disabled = pythonFinal.pythonOlder "3.10";
 
         src = fetchFromGitHub {
           owner = "googleapis";
-          repo = "python-bigquery-magics";
-          rev = "v${version}";
-          hash = "sha256-iGGLivWAQkGIOh7ElCVxq07POwVNOqdqCiP41sSnZuo=";
+          repo = "google-cloud-python";
+          rev = "bigquery-magics-v${version}";
+          hash = "sha256-OjncCE9RQuTO8PZbZNZBMap5Batn8OwNnKp1QYobqAQ=";
         };
+        sourceRoot = "source/packages/bigquery-magics";
 
         propagatedBuildInputs = with pythonPrev; [
           db-dtypes
@@ -27,6 +29,7 @@ in
           pandas
           pyarrow
           pydata-google-auth
+          pyopenssl
           tqdm
         ];
 
@@ -35,7 +38,7 @@ in
 
         meta = with lib; {
           description = "IPython Magics for BigQuery";
-          homepage = "https://github.com/googleapis/python-bigquery-magics";
+          homepage = "https://github.com/googleapis/google-cloud-python/tree/main/packages/bigquery-magics";
           license = licenses.asl20;
         };
       };
