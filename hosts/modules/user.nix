8597caec03474ba840e29e0de75623d0fd073c96
IvanRichter <ivan.richter@anriku.com>
2026-06-13 13:15:04 +0200

    feat(user): add Gibbor source code update script

--- hosts/modules/user.nix
+++ hosts/modules/user.nix
@@ -36,6 +36,35 @@
 
       function fish_greeting
       end
+
+      function gibbor-update --description "Refresh the local Gibbor source checkout"
+        set -l target /home/ivan/work/happy-end/gibbor-src
+        set -l tmp (mktemp -d); or return 1
+        set -l code 0
+
+        scp -P 62884 richter@www.gibbor.eu:~/gibbor_source.tgz "$tmp/gibbor_source.tgz"; and \
+          mkdir "$tmp/extract"; and \
+          tar -xzf "$tmp/gibbor_source.tgz" -C "$tmp/extract"; and \
+          rm -rf "$target"; and \
+          mkdir -p "$target"
+        set code $status
+
+        if test $code -ne 0
+          rm -rf "$tmp"
+          return $code
+        end
+
+        set -l source "$tmp/extract"
+        set -l entries "$source"/*
+        if test (count $entries) -eq 1; and test -d "$entries[1]"
+          set source "$entries[1]"
+        end
+
+        cp -a "$source/." "$target"/
+        set code $status
+        rm -rf "$tmp"
+        return $code
+      end
     '';
     shellAliases = {
       pbcopy = "wl-copy";
